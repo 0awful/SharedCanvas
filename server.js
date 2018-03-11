32de2a30937ff41f248c8d7d34603cc3d3518919
imizaac <im.izaac.seo@gmail.com>
2018-03-10 18:02:13 -0800

    created static file server

---
+++ server.js
@@ -0,0 +1,22 @@
+function handleHTTP(req, res) {
+  if (req.method === 'GET') {
+    req.addListener('end', function() {
+      req.url = req.url.replace(/.*/, '/site.html');
+      static_files.serve(req, res);
+    });
+    req.resume();
+  } else {
+    res.writeHead(403);
+    res.end();
+  }
+}
+
+var host = 'localhost';
+var port = 8080;
+
+var http = require('http');
+var http_serv = http.createServer(handleHTTP).listen(port, host);
+
+var node_static = require('node-static');
+
+var static_files = new node_static.Server(__dirname);
