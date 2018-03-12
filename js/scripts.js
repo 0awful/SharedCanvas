e784f3a21a927a215bd8e87077665fdcd890e94c
imizaac <im.izaac.seo@gmail.com>
2018-03-11 17:27:22 -0700

    Created CSS, made canvas resize

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
 console.log('Scripts loaded');
 
-tools = document.getElementById('tools');
-tools.innerHTML = '<h1> toolPicker </h1>';
+let tools = document.getElementById('tools');
+tools.innerHTML = `
 
-canvas = document.getElementById('canvas');
+<p> red </p>
+<p> blue </p>
+<p> yellow </p>
+<p> green </p>
+<p> purple </p>
+<p> orange </p>
+<p> pink </p>
+<p> teal </p>
+<p> white </p>
+<p> black </p>
+`;
+
+var canvas = document.getElementById('canvas');
+var canvasContainer = document.getElementById('canvasContainer');
+window.onload = window.onresize = function() {
+  canvas.width = canvasContainer.innerWidth;
+  canvas.height = canvasContainer.innerHeight;
+};
+
 console.log(canvas);
