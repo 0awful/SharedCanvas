0c490b5d2a11a5a0201bde7f77f9e8e57fa2e4d2
imizaac <im.izaac.seo@gmail.com>
2018-03-11 18:20:10 -0700

    Drawing and tools are positioned properly Changes were made to make the toolbar fixed so you always have it on screen. Next the canvas was made so that it is fixed in size, you can scroll it. This is going to be a problem when it comes to mobile support. I'll figure it out.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-console.log('Scripts loaded');
-
+// create a tools object that represents the toolbar
 let tools = document.getElementById('tools');
+
+// set this object to contain some basic things
 tools.innerHTML = `
 
 <p> red </p>
@@ -15,11 +16,86 @@ tools.innerHTML = `
 <p> black </p>
 `;
 
-var canvas = document.getElementById('canvas');
-var canvasContainer = document.getElementById('canvasContainer');
-window.onload = window.onresize = function() {
-  canvas.width = canvasContainer.innerWidth;
-  canvas.height = canvasContainer.innerHeight;
-};
+// // get our canvas and it's container
+// var canvas = document.getElementById('canvas');
+// var canvasContainer = document.getElementById('canvasContainer');
+//
 
-console.log(canvas);
+//
+// // get the canvas context
+// context = canvas.getContext('2d');
+
+let canvasWidth = 1000;
+let canvasHeight = 1000;
+
+let canvasDiv = document.getElementById('canvasContainer');
+canvas = document.createElement('canvas');
+canvas.setAttribute('width', canvasWidth);
+canvas.setAttribute('height', canvasHeight);
+canvas.setAttribute('id', 'canvas');
+canvasDiv.appendChild(canvas);
+if (typeof G_vmlCanvasManager != 'undefined') {
+  canvas = G_vmlCanvasManager.initElement(canvas);
+}
+let context = canvas.getContext('2d');
+
+// // make the canvas occupy the container and update whenever the container changes size, like if the browser changes size
+// window.onload = window.onresize = function() {
+//   canvas.width = canvasContainer.innerWidth;
+//   canvas.height = canvasContainer.innerHeight;
+// };
+
+$('#canvas').mousedown(function(e) {
+  var mouseX = e.pageX - this.offsetLeft;
+  var mouseY = e.pageY - this.offsetTop;
+
+  paint = true;
+  addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+  redraw();
+});
+
+$('#canvas').mousemove(function(e) {
+  if (paint) {
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+    redraw();
+  }
+});
+
+$('#canvas').mouseup(function(e) {
+  paint = false;
+});
+
+$('#canvas').mouseleave(function(e) {
+  paint = false;
+});
+
+var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var paint;
+
+function addClick(x, y, dragging) {
+  clickX.push(x);
+  clickY.push(y);
+  clickDrag.push(dragging);
+}
+
+function redraw() {
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+
+  context.strokeStyle = '#df4b26';
+  context.lineJoin = 'round';
+  context.lineWidth = 5;
+
+  for (var i = 0; i < clickX.length; i++) {
+    context.beginPath();
+    if (clickDrag[i] && i) {
+      context.moveTo(clickX[i - 1], clickY[i - 1]);
+    } else {
+      context.moveTo(clickX[i] - 1, clickY[i]);
+    }
+    context.lineTo(clickX[i], clickY[i]);
+    context.closePath();
+    context.stroke();
+  }
+}
