786d56d926b3ba1665b411085503b583d2199818
imizaac <im.izaac.seo@gmail.com>
2018-04-04 13:00:52 -0700

    updated for ESLintaAirbnb

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
+const host = 'localhost';
+const port = 8080;
+
+const http = require('http');
+
+const nodeStatic = require('node-static');
+
+const staticFiles = new nodeStatic.Server(__dirname);
+
+let drawings = {};
+const sockets = [];
+
 function handleHTTP(req, res) {
   if (req.method === 'GET') {
     // TODO: DEFINITELY REPLACE THIS WITH AN AUTHENTICATED WAY OF DOING THIS
-    if (req.url == '/resetcanvas') {
+    if (req.url === '/resetcanvas') {
       drawings = [];
     }
     if (req.url === '/') {
-      req.addListener('end', function() {
+      req.addListener('end', () => {
         req.url = req.url.replace(/.*/, '/site.html');
-        static_files.serve(req, res);
+        staticFiles.serve(req, res);
       });
       req.resume();
     } else {
-      static_files.serve(req, res);
+      staticFiles.serve(req, res);
       req.resume();
     }
   } else {
@@ -22,35 +34,21 @@ function handleHTTP(req, res) {
 
 function handleIO(socket) {
   function disconnect() {
-    console.log('client disconnected');
     sockets.splice(sockets.indexOf(socket), 1);
   }
 
   sockets.push(socket);
-  console.log('client Connected');
-  console.log(sockets.length);
   socket.emit('updateDrawings', drawings);
 
   socket.on('disconnect', disconnect);
-  socket.on('drawing', function(key, drawing) {
+  socket.on('drawing', (key, drawing) => {
     drawings[key] = drawing;
     socket.broadcast.emit('drawing', key, drawing);
   });
 }
 
-let host = 'localhost';
-let port = 8080;
+const httpServe = http.createServer(handleHTTP).listen(port, host);
 
-let http = require('http');
-let http_serv = http.createServer(handleHTTP).listen(port, host);
-
-let node_static = require('node-static');
-
-let static_files = new node_static.Server(__dirname);
-
-let io = require('socket.io').listen(http_serv);
+const io = require('socket.io').listen(httpServe);
 
 io.on('connection', handleIO);
-
-let drawings = {};
-let sockets = new Array();
