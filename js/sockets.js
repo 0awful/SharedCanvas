be2d7f9b6c076af5ac7f9b0a8fee94a51ddee4dc
imizaac <im.izaac.seo@gmail.com>
2018-04-14 01:10:12 -0700

    added variable duration

--- js/sockets.js
+++ js/sockets.js
@@ -2,8 +2,8 @@ import openSocket from 'socket.io-client';
 
 const socket = openSocket('/');
 
-function subscribeToTimer(cb) {
+function subscribeToTimer(timerDuration, cb) {
   socket.on('timer', timestamp => cb(null, timestamp));
-  socket.emit('subscribeToTimer', 1000);
+  socket.emit('subscribeToTimer', 1000, timerDuration);
 }
 export default subscribeToTimer;
