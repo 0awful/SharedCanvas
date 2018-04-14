ae597d965dc1d930d98afa8e1193916eb623595f
imizaac <im.izaac.seo@gmail.com>
2018-04-14 01:11:20 -0700

    created a function that updates the store for timervalue

---
+++ js/updateTimer.js
@@ -0,0 +1,8 @@
+import { setTimerValue } from './actionCreators';
+import store from './store';
+
+const updateTimer = newValue => {
+  store.dispatch(setTimerValue(newValue));
+};
+
+export default updateTimer;
