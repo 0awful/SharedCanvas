8c01d9d6f734e79d67256dfb30805370d46b2765
imizaac <im.izaac.seo@gmail.com>
2018-04-14 02:00:03 -0700

    minor refactor

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,14 +2,13 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import App from './App';
+import App from './Components/App';
 
 const renderApp = () => {
   const htmlElement = document.getElementById('app');
   if (htmlElement) {
     return render(<App />, htmlElement);
-  } 
-    return <p>404</p>;
-  
+  }
+  return <p>404</p>;
 };
 renderApp();
