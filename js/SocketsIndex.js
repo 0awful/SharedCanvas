400a8b3c4c7fbb7dd7e0330ff19333b61831ab6a
imizaac <im.izaac.seo@gmail.com>
2018-04-14 04:09:47 -0700

    syntactic sugar for socket related functions

--- js/SocketsIndex.js
+++ js/SocketsIndex.js
@@ -1,5 +1,7 @@
 import subscribeToTimer from './Sockets/sockets';
 import updateTimer from './Sockets/updateTimer';
+import updateKey from './Sockets/fetchKey';
 
 export { subscribeToTimer };
 export { updateTimer };
+export { updateKey };
