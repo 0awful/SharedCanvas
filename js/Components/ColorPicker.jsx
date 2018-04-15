de209d7d3ac8be2b540665456cde6e96af7afdf9
imizaac <im.izaac.seo@gmail.com>
2018-04-15 14:57:05 -0700

    made color picker Each click gives you a new random color, but you can never go back!

--- js/Components/ColorPicker.jsx
+++ js/Components/ColorPicker.jsx
@@ -1,6 +1,8 @@
 // @flow
 import React from 'react';
+import { connect } from 'react-redux';
 import Paper from 'material-ui/Paper';
+import { setBrushColor } from '../actionCreators';
 
 const colorPickerStyle = {
   left: 0,
@@ -13,19 +15,58 @@ const colorPickerStyle = {
   textAlign: 'center',
   display: 'grid',
   gridTemplateRows: 'repeat(3,1fr)',
-  gridTemplateColumns: 'repeat(3,1fr)'
+  gridTemplateColumns: '10% auto 10%'
 };
 
-// const pickerStyle = {
-//   gridColumnStart: 2,
-//   gridRowStart: 2,
-//   fontSize: '20px'
-// };
+const pickerStyle = {
+  gridColumnStart: 2,
+  gridRowStart: 2,
+  fontSize: '20px'
+};
 
-const ColorPicker = () => (
-  <Paper id="ColorPickerBody" style={colorPickerStyle} zDepth={4} circle>
-    <input type="color" />
-  </Paper>
-);
+const randomColor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`;
+};
 
-export default ColorPicker;
+const ColorPicker = (props: {
+  brushColor: string,
+  brushColorDefault: string,
+  handleBrushColorChange: string => void
+}) => {
+  const clickFunction = () => {
+    props.handleBrushColorChange(randomColor());
+  };
+
+  let selectorText = '';
+  if (props.brushColor === props.brushColorDefault) {
+    selectorText = 'Click Me!';
+  }
+  colorPickerStyle.backgroundColor = props.brushColor;
+  return (
+    <Paper
+      onClick={clickFunction}
+      id="ColorPickerBody"
+      style={colorPickerStyle}
+      zDepth={4}
+      circle
+    >
+      <p style={pickerStyle}>{selectorText}</p>
+    </Paper>
+  );
+};
+
+const mapDispatchToProps = dispatch => ({
+  handleBrushColorChange(color) {
+    dispatch(setBrushColor(color));
+  }
+});
+
+const mapStateToProps = state => ({
+  brushColor: state.brushColor,
+  brushColorDefault: state.brushColorDefault
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ColorPicker);
