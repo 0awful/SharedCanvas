965a2ca60c59e6b42be5319b6ea8838383821b51
imizaac <im.izaac.seo@gmail.com>
2018-04-23 22:26:42 -0700

    added flow no problems

--- js/Sockets/sockets.js
+++ js/Sockets/sockets.js
@@ -3,7 +3,10 @@ import openSocket from 'socket.io-client';
 
 const socket = openSocket('/');
 
-function subscribeToTimer(timerDuration, cb) {
+function subscribeToTimer(
+  timerDuration: number,
+  cb: (?string, number) => void
+) {
   socket.on('timer', timerValue => cb(null, timerValue));
   socket.emit('subscribeToTimer', 1000, timerDuration);
 }
@@ -11,22 +14,26 @@ function subscribeToTimer(timerDuration, cb) {
 function requestKey() {
   socket.emit('requestKey');
 
+  // $FlowFixMe dunno how it wants me to denote this
   const keyPromise = new Promise(resolve => {
     socket.on('key', key => resolve(key));
   });
   return keyPromise;
 }
 
-function emitDrawing(key, drawing) {
+function emitDrawing(key: string, drawing: Drawing | false) {
   socket.emit('drawing', key, drawing);
 }
 
-const connect = (updateDrawingsCB, newDrawingCB) => {
+const connect = (
+  updateDrawingsCB: ({ string: [Drawing] }) => void,
+  newDrawingCB: (string, Drawing) => void
+) => {
   socket.on('updateDrawings', drawings => {
     updateDrawingsCB(drawings);
   });
 
-  socket.on('drawing', (key, drawing) => {
+  socket.on('drawing', (key: string, drawing: Drawing) => {
     newDrawingCB(key, drawing);
   });
 };
