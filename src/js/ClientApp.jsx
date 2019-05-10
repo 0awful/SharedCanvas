dbf4db831de2635afd20e4efc074e29312e82adf
Izaac Wilkowski <im.izaac.seo@gmail.com>
2019-05-09 19:54:30 -0700

    moved files

--- src/js/ClientApp.jsx
+++ src/js/ClientApp.jsx
@@ -4,11 +4,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './ComponentsIndex';
 
-const renderApp = () => {
+export default () => {
   const htmlElement = document.getElementById('app');
   if (htmlElement) {
     return render(<App />, htmlElement);
   }
   return <p>404</p>;
 };
-renderApp();
